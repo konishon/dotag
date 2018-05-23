b533e98c633570a8b381e37bf0b987a5b7fe50d4
nishon.tan <nishon.tan@gmail.com>
2018-05-23 09:00:37 +0545

    Adds requirements txt

--- apiv2/permissions.py
+++ apiv2/permissions.py
@@ -1,8 +1,8 @@
 from rest_framework.permissions import BasePermission
-from .models import Bucketlist
+from .models import ReportRating
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if isinstance(obj, Bucketlist):
+        if isinstance(obj, ReportRating):
             return obj.reporter == request.user
         return obj.reporter == request.user
